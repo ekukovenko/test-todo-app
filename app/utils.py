e459a15c357b522be658577d9f9f4e88e16d8943
Ivan Litvinov <ivan.litvinov7898@gmail.com>
2026-01-30 02:48:49 +0300

    Add subtract function to app/utils.py

---
+++ app/utils.py
@@ -0,0 +1,12 @@
+def subtract(a: int, b: int) -> int:
+    """
+    Subtracts two numbers.
+
+    Args:
+        a: The first number.
+        b: The second number to subtract from the first.
+
+    Returns:
+        The result of the subtraction.
+    """
+    return a - b

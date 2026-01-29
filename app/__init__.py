7d4427bfb305aec462f4628cf588a674576bad44
Ivan Litvinov <ivan.litvinov7898@gmail.com>
2026-01-30 01:21:26 +0300

    Добавлена функция get_version() и константа __version__ в app/__init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,12 @@
 # TODO App
+
+__version__ = "1.0.0"
+
+def get_version() -> str:
+    """
+    Возвращает текущую версию приложения.
+
+    Returns:
+        str: Строка с номером версии.
+    """
+    return __version__

4937e80012cd6261763062cfd9924a70ddd33f1f
Ivan Litvinov <ivan.litvinov7898@gmail.com>
2026-01-30 03:33:36 +0300

    Add square function to app/utils.py

---
+++ app/utils.py
@@ -0,0 +1,11 @@
+def square(x: float) -> float:
+    """
+    Возводит число в квадрат.
+
+    Args:
+        x (float): Число для возведения в квадрат.
+
+    Returns:
+        float: Квадрат числа x.
+    """
+    return x * x
